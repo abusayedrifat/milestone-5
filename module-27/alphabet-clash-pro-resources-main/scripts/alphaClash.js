26a066b5c3d501ab32044d199c2210c1c318c86d
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-02-18 09:03:42 +0600

    alpha clash

--- module-27/alphabet-clash-pro-resources-main/scripts/alphaClash.js
+++ module-27/alphabet-clash-pro-resources-main/scripts/alphaClash.js
@@ -9,12 +9,70 @@
 //     // scoreBoard.classList.remove('hidden')
 // }
 
+function handleKeyboardButtonPress(event) {
+    const keyPressed = event.key;
+
+//stop the game
+if (keyPressed === 'Escape') {
+    gameOver();
+}
+
+
+    // console.log( keyPressed);
+    //get the expected to press
+const currentAlphabetElement = document.getElementById('current-alphabet');
+const expectedAlphabet = currentAlphabetElement.innerText.toLowerCase()
+console.log(keyPressed , expectedAlphabet);
+
+//checked match or not
+
+
+if (keyPressed === expectedAlphabet) {
+    console.log('you got a point');
+    //update score
+    //1.get the current score
+    const currentScoreElement = document.getElementById('current-score');
+
+    const currentScore = parseInt(currentScoreElement.innerText );
+    //2.increase the score by 1
+    const newScore = currentScore+1 ;
+    // show the update score
+    currentScoreElement.innerText = newScore;
+    //start new round
+    continueGame();
+    removeBackgroundColorById(expectedAlphabet);
+}
+else{
+    
+    console.log('you lost a life');
+
+    //step-1: get the current life number
+ const currentLifeElement = document.getElementById('current-life');
+const currentLife = parseInt(currentLifeElement.innerText);
+    //2. reduce the current life
+const newLifeStaus = currentLife - 1;
+    //3.update life status
+  const lifeStatus =  currentLifeElement.innerText = newLifeStaus
+
+    if (lifeStatus === 0) {
+       gameOver();
+    }
+}
+}
+
+
+
+
+
+
+//capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
 
 
 function continueGame() {
     //generate random alphabet
     const alphabet = getRandomALphabet();
- console.log('your random alphat is : ',alphabet);
+ console.log('your random alphabet is : ',alphabet);
 
  //set randomly generated alphabet to the screen;
  const randomAlphabet = document.getElementById('current-alphabet');
@@ -27,7 +85,25 @@ function continueGame() {
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    
     continueGame();
 }
 
-
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('score-board');
+    //update final score
+    const gameScore = getTextElementValueById('current-score');
+    // console.log(gameScore);
+    setTextElementValueById('game-score', gameScore);
+    // const currentAlphabet =  getElementTextById('current-alphabet')
+    // removeBackgroundColorById(currentAlphabet);
+    removeBackgroundColorById('current-alphabet')
+}
+//play again btn
+function playAgain() {
+    hideElementById('score-board');
+    showElementById('play-ground');
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
+}
--- module-27/alphabet-clash-pro-resources-main/utility/utility.js
+++ module-27/alphabet-clash-pro-resources-main/utility/utility.js
@@ -1,3 +1,5 @@
+
+
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
@@ -8,10 +10,32 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
+
 //alphabet color
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
+}
+//remove alphabet color
+function  removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const value =parseInt(element.innerText);
+    return value;
+}
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value ;
+}
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text
 }
 
 //alphabet clash script
